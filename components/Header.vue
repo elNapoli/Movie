c6f25c8f87ffb0074cb7c6e82b14403f47688371
elnapoli <baldomero.napoli@gmail.com>
2024-04-13 19:31:46 -0500

    add Wuasar
    1. added Quasar
    2. new components:
      2.1 drawer
      2.2 header

---
+++ components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <q-header elevated class="bg-grey-6">
+        <q-toolbar>
+            <q-btn
+                flat
+                dense
+                round
+                @click="handleButtonClick"
+                aria-label="Menu"
+                icon="menu"
+            />
+
+            <q-toolbar-title> Quasar App </q-toolbar-title>
+        </q-toolbar>
+    </q-header>
+</template>
+<script setup>
+const emit = defineEmits(['toggleDrawer'])
+
+const handleButtonClick = () => {
+    emit('toggleDrawer')
+}
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ['@nuxtjs/tailwindcss'],
+    modules: ['nuxt-quasar-vite'],
+    quasar: {
+        // Optional string | boolean
+        // Optional string[]
+        // If you use animations, add Quasar Extra CSS animation URL here.
+        // NOTE: This CSS files are inserted on module plugin template, NOT on `nuxt.config`.
+        css: ['@quasar/extras/material-icons/material-icons.css'],
+        // List of extra Quasar Plugins
+        // auto-instaled: [Platform, Body, Dark, Screen, History, Lang, IconSet]
+        // optional: [AddressbarColor, AppFullscreen, AppVisibility, BottomSheet, Dialog,
+        //            LoadingBar, Loading, Notify, LocalStorage, SessionStorage]
+        plugins: ['Dialog'],
+        /* Quasar UI config -- you'll notice in Quasar docs when you need it */
+        config: {
+            dark: false,
+        },
+    },
 })
