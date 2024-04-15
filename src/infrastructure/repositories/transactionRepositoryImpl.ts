4d60dc28a056cca7e182d935c1081104aa7b932c
elnapoli <baldomero.napoli@gmail.com>
2024-04-15 10:07:17 -0500

    backup
    i will do a backup cause i need re-structure the project again

--- src/infrastructure/repositories/transactionRepositoryImpl.ts
+++ src/infrastructure/repositories/transactionRepositoryImpl.ts
@@ -1,13 +1,13 @@
 import { Transaction } from '../../domain/models/transaction'
 import { TransactionRepository } from '../../domain/repositories/TransactionRepository'
-import { Http } from '../../domain/repositories/HttpMethods'
+import { Http } from '../../domain/repositories/Http'
 import { TransactionDto } from '../../infrastructure/http/dto/TransactionDto'
 
 export const transactionRepositoryImpl = (
     client: Http
 ): TransactionRepository => ({
     getTransactions: async () => {
-        const transactions = await client.get<TransactionDto>('')
+        const transactions = await client.get<TransactionDto>('transactions')
         return transactions.map(
             (transactionDto): Transaction => ({
                 id: transactionDto.id,
