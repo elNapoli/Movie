ae02a869e12b7ffdc920df8202ebf1ec2a5112dd
elnapoli <baldomero.napoli@gmail.com>
2024-04-16 17:41:02 -0500

    add login with google
    google provider

--- src/presenter/components/Header.vue
+++ src/presenter/components/Header.vue
@@ -10,14 +10,51 @@
                 icon="menu"
             />
 
-            <q-toolbar-title> Quasar App </q-toolbar-title>
+            <q-toolbar-title> Super Lunes </q-toolbar-title>
+
+            <q-btn
+                flat
+                dense
+                @click="handleSessionClick"
+                :icon-right="user === null ? 'login' : 'logout'"
+                aria-label="Login"
+                :label="user === null ? '' : user.email"
+            />
         </q-toolbar>
     </q-header>
 </template>
 <script setup>
+const supabase = useSupabaseClient()
 const emit = defineEmits(['toggleDrawer'])
-
+const { data } = await supabase.auth.getUser()
+console.log(data)
+const user = ref(data.user)
 const handleButtonClick = () => {
     emit('toggleDrawer')
+}
+
+const handleSessionClick = () => {
+    if (user.value === null) {
+        signIn()
+    } else {
+        signOut()
+    }
+}
+
+const signIn = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'google',
+        options: {
+            queryParams: {
+                access_type: 'offline',
+                prompt: 'consent',
+            },
+        },
+    })
+}
+
+const signOut = async () => {
+    const { error } = await supabase.auth.signOut()
+    user.value = null
 }
 </script>
--- src/presenter/pages/index.vue
+++ src/presenter/pages/index.vue
@@ -2,15 +2,4 @@
     <div>hola</div>
 </template>
 
-<script setup lang="ts">
-const supabase = useSupabaseClient()
-const { data, error } = await supabase.auth.signInWithOAuth({
-    provider: 'google',
-    options: {
-        queryParams: {
-            access_type: 'offline',
-            prompt: 'consent',
-        },
-    },
-})
-</script>
+<script setup lang="ts"></script>
