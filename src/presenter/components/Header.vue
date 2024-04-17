410c35de71bb1d8b7308aa1e9badef5d4f6981b6
elnapoli <baldomero.napoli@gmail.com>
2024-04-17 16:44:40 -0500

    add login with googlw

--- src/presenter/components/Header.vue
+++ src/presenter/components/Header.vue
@@ -3,14 +3,44 @@
         <v-app-bar-nav-icon @click="handleButtonClick"></v-app-bar-nav-icon>
 
         <v-app-bar-title>Super Lunes</v-app-bar-title>
-        <v-btn icon="mdi-login"> </v-btn>
+        {{ user ? user.email : '' }}
+        <v-btn
+            :icon="user ? 'mdi-logout' : 'mdi-login'"
+            @click="handleSessionClick"
+        >
+        </v-btn>
     </v-app-bar>
 </template>
 <script setup>
+const supabase = useSupabaseClient()
 const emit = defineEmits(['click'])
-
+const { data } = await supabase.auth.getUser()
+const user = ref(data.user)
 const handleButtonClick = () => {
-    console.log('adsfasdf')
     emit('click')
+}
+const handleSessionClick = () => {
+    if (user.value === null) {
+        signIn()
+    } else {
+        signOut()
+    }
+}
+
+const signIn = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'google',
+        options: {
+            queryParams: {
+                access_type: 'offline',
+                prompt: 'consent',
+            },
+        },
+    })
+}
+
+const signOut = async () => {
+    const { error } = await supabase.auth.signOut()
+    user.value = null
 }
 </script>
